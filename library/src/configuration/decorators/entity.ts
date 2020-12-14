fd935cedb1ae87d2904d6c74aca049be26a06107
Roman Yatsuniak <romanyatsuniak@gmail.com>
2020-12-14 10:49:34 +0200

    fix: tableName and constructor prototype configurations

--- library/src/configuration/decorators/entity.ts
+++ library/src/configuration/decorators/entity.ts
@@ -5,16 +5,12 @@ import { TableMap } from '../../common/models/table-map';
 export function Entity(tables: Tables, settings?: EntityConfiguration) {
   return function (constructor: () => void): void {
     const dataMap: TableMap = {
-      tableName: 'Avasdasdbsd',
+      tableName: settings ? settings.tableName : constructor.name,
       columns: [],
       constructor,
     };
-    if (settings) {
-      dataMap.tableName = settings.tableName;
-      constructor.prototype['_orm_table_name'] = settings.tableName;
-    } else {
-      constructor.prototype['_orm_table_name'] = constructor.name;
-    }
+    constructor.prototype['_orm_table_name'] = dataMap.tableName;
+
     tables.add(dataMap);
   };
 }
