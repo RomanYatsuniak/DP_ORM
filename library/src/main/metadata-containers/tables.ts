f22ea6974aea47758383d1fc4e600cd2fe83d0d2
HubMiz <hubmiz20@gmail.com>
2020-12-14 21:08:27 +0100

    Changed getNames,removed ToDo,guard clauses in get

--- library/src/main/metadata-containers/tables.ts
+++ library/src/main/metadata-containers/tables.ts
@@ -5,33 +5,17 @@ export class Tables {
   private nameToTableMap: Map<string, TableMap> = new Map();
 
   add(element: TableMap): void {
-    // TODO
-    // should insert element into tableMaps and nameToTableMap
     this.tableMaps.push(element);
-    this.nameToTableMap.set(element.tableName,element);
+    this.nameToTableMap.set(element.tableName, element);
   }
 
   get(name: string): TableMap {
-    // TODO
-    // should returns map with given name
-    // if not exist -> throw error
+    if (this.nameToTableMap.has(name)) return this.nameToTableMap.get(name);
 
-    if(this.nameToTableMap.has(name)){
-      return this.nameToTableMap.get(name);
-    }
-    else{
-      throw new Error("There is no entry with that name")
-    }
-
+    throw new Error('There is no TableMap with that name');
   }
 
   getNames(): string[] {
-    // TODO
-    // should returns array of names of all tables
-    let arrayOfNames: string[] = [];
-    for (let [key, value] of this.nameToTableMap) {
-      arrayOfNames.push(key);
-    }
-    return arrayOfNames;
+    return Array.from(this.nameToTableMap.keys());
   }
 }
